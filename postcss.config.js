15ac17596527d032a26d0de46871ba192a348da0
takiguchi-yu <y.takiguti@gmail.com>
2020-08-12 09:34:13 +0900

    assets-metadata-css-starter

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+module.export = {
+  plugins: [
+    'tailwindcss',
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009',
+        },
+        stage: 3, 
+        feature: {
+          'custom-properties': false
+        }
+      }
+    ]
+  ]
+}
